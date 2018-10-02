f042b86d264fa5b05f770ab326b5f2f064aebdef
vagrant <vagrant@localhost.localdomain>
2018-10-02 19:19:26 +0000

    used node object to access host properties using string interpolation

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,7 +5,10 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-   content '<h1>Hello, world!</h1>'
+   content "<h1>Hello, world!</h1>
+	<h2>Your IP is #{node['ipaddress']}</h2>
+	<h2>Your hostname is #{node['hostname']}</h2>   
+"
 end
 
 service 'httpd' do
