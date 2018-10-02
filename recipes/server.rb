56c60a1d3a45caa9c54717766e4aeee0a4bcc1c6
vagrant <vagrant@localhost.localdomain>
2018-10-02 20:09:22 +0000

    refactored server recipe to use erb templates

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,11 +4,11 @@ package 'httpd' do
    action :install
 end
 
-file '/var/www/html/index.html' do
-   content "<h1>Hello, world!</h1>
-	<h2>Your IP is #{node['ipaddress']}</h2>
-	<h2>Your hostname is #{node['hostname']}</h2>   
-"
+template  '/var/www/html/index.html' do
+   source 'index.erb'
+   variables(
+	:name => 'teekay'
+   )
 end
 
 service 'httpd' do
